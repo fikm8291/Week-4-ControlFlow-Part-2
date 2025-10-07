65d6139f9fba4f085b6a00b4ed03b18b95159cb7
Tony Huynh <tony.huynh@manadulted.ac.uk>
2025-10-07 04:02:26 +0100

    Create Ex2_conditionals.py

---
+++ Ex2_conditionals.py
@@ -0,0 +1,118 @@
+# -------------------------------------------
+# Exercise 2: Conditional Statements
+# -------------------------------------------
+# In this exercise, you will learn how to use conditional statements in Python.
+# Conditional statements let your program make decisions based on True or False values.
+#
+# Key keywords:
+# - if    : start a condition
+# - elif  : check another condition if the first condition is False
+# - else  : run code if no other conditions are True
+#
+# -------------------------------------------
+
+# Step 1: Simple if statement
+# ---------------------------
+# Start with one condition.
+
+# Example of a simple if statement (just syntax):
+# if some_condition:
+#     print("Write your message here")
+
+# TODO:
+# 1. Create a variable called `number` and give it a value.
+# 2. Write an if statement to check if the number is greater than 0.
+# 3. Print a message if the condition is True.
+
+# Write your code below:
+
+
+# Step 2: if-else statement
+# -------------------------
+# Sometimes you want to do one thing if the condition is True, 
+# and something else if it is False.
+
+# Example (just syntax):
+# if some_condition:
+#     print("Message if True")
+# else:
+#     print("Message if False")
+
+# TODO:
+# 1. Change your code from Step 1 to add an else statement.
+# 2. Print a different message if the number is 0 or less.
+
+# Write your code below:
+
+
+# Step 3: if-elif-else statement
+# ------------------------------
+# Use elif to check more than one condition.
+
+# Example (just syntax):
+# if condition1:
+#     print("Message 1")
+# elif condition2:
+#     print("Message 2")
+# else:
+#     print("Message 3")
+
+# TODO:
+# 1. Change your code to use an elif statement.
+# 2. Check three cases: positive number, zero, negative number.
+# 3. Print your own messages for each case.
+
+# Write your code below:
+
+
+# -------------------------------------------
+# Submitting Your Work
+# -------------------------------------------
+# When you have finished this exercise:
+# 1. Save your file
+# 2. Open the terminal
+# 3. Run the following commands:
+#    git add Ex2_conditionals.py
+#    git commit -m "Completed conditional statements exercise"
+#    git push origin main
+# Check GitHub to see your changes.
+# -------------------------------------------
+
+# -------------------------------------------
+# EXTENSION ACTIVITIES
+# -------------------------------------------
+
+# Extension 1:
+# Ask the user to type a number.
+# Use if-elif-else to print a message depending on the number.
+
+# Extension 2:
+# Ask the user to type their age.
+# Use if-elif-else to print messages for different age groups:
+# - under 13
+# - 13 to 17
+# - 18 to 64
+# - 65 or older
+
+# Extension 3 (more challenging):
+# Ask the user to type a username and a password.
+# - If both are correct, print a success message.
+# - If the username is correct but the password is wrong, print a different message.
+# - If the username is wrong, print another message.
+# Hint: You may need nested if statements or multiple conditions with and/or.
+
+# Write your extension code below:
+
+
+# -------------------------------------------
+# Submitting Your Work
+# -------------------------------------------
+# When you have finished this exercise:
+# 1. Save your file
+# 2. Open the terminal
+# 3. Run the following commands:
+#    git add Ex2_conditionals.py
+#    git commit -m "Completed extension exercises"
+#    git push origin main
+# Check GitHub to see your changes.
+# -------------------------------------------
